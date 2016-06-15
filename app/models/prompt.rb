7341a5e1474c2d4d261fea15908a54929d19df87
djres88 <davidj.resnick@gmail.com>
2016-06-15 14:10:31 -0700

    Round one of prompt generator complete. Let's add more words to the seedfile

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -11,35 +11,36 @@
 
 class Prompt < ActiveRecord::Base
   validates :word, presence: true, uniqueness: true
-  validates :part_of_speech, presence: true, inclusion: {in: ['occupation', 'noun', 'adjective', 'event']}
+  validates :part_of_speech, presence: true, inclusion: {in: ['living thing', 'object', 'adjective', 'event']}
 
-  def is_vowel(let)
+  def is_vowel?(let)
     ["a", "e", "i", "o", "u"].include?(let)
   end
 
-  def random_prompt(all_words)
+  def random_prompt(dict)
     # Put words into a hash, where keys are part of speech. Single O(n) operation allows for constant random sampling.
-    words_hash = Hash.new([])
-    all_words.each do |word|
-      words_hash[part_of_speech] << word
+    words_hash = Hash.new
+    dict.each do |entry|
+      key = entry[:part_of_speech]
+      words_hash[key] = [] unless words_hash[key]
+      words_hash[key].push(entry[:word])
     end
-
     # Generate prompt
     prompt = "Your story begins with "
 
-    part1 = words_hash['adjective'].sample + words_hash['occupation'].sample
-    part1 = is_vowel(part1[0]) ? "an" + part1 : "a" + part1
+    part1 = words_hash['adjective'].sample +  " " + words_hash['living thing'].sample
+    part1 = is_vowel?(part1[0]) ? "an " + part1 : "a " + part1
 
-    part2 = words_hash['noun'].sample
-    part2 = is_vowel(part2[0]) ? "an" + part2 : "a" + part2
+    part2 = words_hash['object'].sample
+    part2 = is_vowel?(part2[0]) ? "an " + part2 : "a " + part2
 
-    partfinal = words_hash['adjective'].sample + words_hash['event'].sample
-    partfinal = is_vowel(part2[0]) ? "an" + part2 : "a" + part2
+    partfinal = words_hash['adjective'].sample + " " + words_hash['event'].sample
+    partfinal = is_vowel?(partfinal[0]) ? "an " + partfinal : "a " + partfinal
 
     if rand > 0.5
-      prompt += (part1 + "," + part2 + "," + partfinal)
+      prompt += (part1 + ", " + part2 + ", and " + partfinal)
     else
-      prompt += (part2 + "," + part1 + "," + partfinal)
+      prompt += (part2 + ", " + part1 + ", and " + partfinal)
     end
     prompt
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,45 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
- 
+
+Prompt.create([
+  { word: 'mischievous', part_of_speech: 'adjective' },
+  { word: 'good', part_of_speech: 'adjective' },
+  { word: 'bad', part_of_speech: 'adjective' },
+  { word: 'little', part_of_speech: 'adjective' },
+  { word: 'big', part_of_speech: 'adjective' },
+  { word: 'important', part_of_speech: 'adjective' },
+  { word: 'gnarly', part_of_speech: 'adjective' },
+  { word: 'gassy', part_of_speech: 'adjective' },
+  { word: 'heartfelt', part_of_speech: 'adjective' },
+  { word: 'unbelievable', part_of_speech: 'adjective' },
+  { word: 'dog', part_of_speech: 'living thing' },
+  { word: 'carpenter', part_of_speech: 'living thing' },
+  { word: 'fish', part_of_speech: 'living thing' },
+  { word: 'brick layer', part_of_speech: 'living thing' },
+  { word: 'longshoreman', part_of_speech: 'living thing' },
+  { word: 'maid', part_of_speech: 'living thing' },
+  { word: 'CEO', part_of_speech: 'living thing' },
+  { word: 'farter', part_of_speech: 'living thing' },
+  { word: 'programmer', part_of_speech: 'living thing' },
+  { word: 'lawyer', part_of_speech: 'living thing' },
+  { word: 'pickle', part_of_speech: 'object' },
+  { word: 'motel', part_of_speech: 'object' },
+  { word: 'cliff', part_of_speech: 'object' },
+  { word: 'country', part_of_speech: 'object' },
+  { word: 'farm', part_of_speech: 'object' },
+  { word: 'airplane', part_of_speech: 'object' },
+  { word: 'race car', part_of_speech: 'object' },
+  { word: 'shopping mall', part_of_speech: 'object' },
+  { word: 'box social', part_of_speech: 'object' },
+  { word: 'parade', part_of_speech: 'event' },
+  { word: 'date', part_of_speech: 'event' },
+  { word: 'movie', part_of_speech: 'event' },
+  { word: 'rocket launch', part_of_speech: 'event' },
+  { word: 'murder plot', part_of_speech: 'event' },
+  { word: 'night', part_of_speech: 'event' },
+  { word: 'surfing competition', part_of_speech: 'event' },
+  { word: 'breakfast', part_of_speech: 'event' },
+  { word: 'rave', part_of_speech: 'event' },
+  { word: 'film shoot', part_of_speech: 'event' }
+])
