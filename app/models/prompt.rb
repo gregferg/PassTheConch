381cea41edd75ddddbdc694a8621c3732ee5d10d
djres88 <davidj.resnick@gmail.com>
2016-06-15 12:43:06 -0700

    Random prompt generator, version 1

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -11,5 +11,23 @@
 
 class Prompt < ActiveRecord::Base
   validates :word, presence: true, uniqueness: true
-  validates :part_of_speech, presence: true, inclusion: {in: ['Person', 'Occupation', 'Place', 'Object', 'Adjective', 'Event']}
+  validates :part_of_speech, presence: true, inclusion: {in: ['occupation', 'noun', 'adjective', 'event']}
+
+  def random_prompt(all_words)
+    words_hash = Hash.new([])
+    all_words.each do |word|
+      words_hash[part_of_speech] << word
+    end
+
+    prompt = "Your story begins with "
+    part1 = words_hash['occupation'].sample + words_hash['adjective'].sample
+    part2 = words_hash['noun'].sample
+    partfinal = words_hash['adjective'].sample + words_hash['event'].sample
+    if rand > 0.5
+      prompt += (part1 + part2 + partfinal)
+    else
+      prompt += (part2 + part1 + partfinal)
+    end
+    prompt
+  end
 end
